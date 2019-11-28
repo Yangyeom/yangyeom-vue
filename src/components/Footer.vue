5e489ff56fcfa36e3693e5d6231403bd75baef28
yeomkyeorae <duarufp06@naver.com>
2019-11-29 07:44:30 +0900

    HJ | Vue | Editted the emergency button a href

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -17,25 +17,17 @@
                             <p class="text-muted">CEO</p>
                             <ul class="list-inline social-buttons">
                                 <li class="list-inline-item">
-                                    <a href="">
+                                    <a href="#">
                                         <i class="fab fa-twitter"></i>
                                     </a>
                                 </li>
                                 <li class="list-inline-item">
-<<<<<<< Updated upstream
-                                    <a @click.prevent="$EventBus.$emit('emergency')">
-=======
-<<<<<<< Updated upstream
-                                    <a href="#">
-=======
-                                    <a href="" @click.prevent="$EventBus.$emit('emergency')">
->>>>>>> Stashed changes
->>>>>>> Stashed changes
+                                    <a href="#" @click.prevent="$EventBus.$emit('emergency')">
                                         <i class="fab fa-facebook-f"></i>
                                     </a>
                                 </li>
                                 <li class="list-inline-item">
-                                    <a href="">
+                                    <a href="#">
                                         <i class="fab fa-linkedin-in"></i>
                                     </a>
                                 </li>
@@ -49,17 +41,17 @@
                             <p class="text-muted">CTO</p>
                             <ul class="list-inline social-buttons">
                                 <li class="list-inline-item">
-                                    <a href="">
+                                    <a href="#">
                                         <i class="fab fa-twitter"></i>
                                     </a>
                                 </li>
                                 <li class="list-inline-item">
-                                    <a href="">
+                                    <a href="#">
                                         <i class="fab fa-facebook-f"></i>
                                     </a>
                                 </li>
                                 <li class="list-inline-item">
-                                    <a href="">
+                                    <a href="#">
                                         <i class="fab fa-linkedin-in"></i>
                                     </a>
                                 </li>
